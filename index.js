02dc0090c05ba96d24d26e65b396336c7f83d0be
EmRodr <emanuelrodriguez203@gmail.com>
2024-01-23 19:36:48 -0300

    Added the helpers to the engine, added cookieParser to the project, parameterized the project's port, defined the session

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+const mongoose = require('mongoose');
+require('./config/db');
+
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 
+require('dotenv').config({path: '.env'});
+
 
 const router = require('./routes');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+//Enable body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Enable handlebars as a view
 
 app.engine('handlebars',
     exphbs.engine({
-        defaultLayout: 'layout'
+        defaultLayout: 'layout',
+        helpers: require('./helpers/handlebars')
     })
 );
 
@@ -20,6 +35,16 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl: process.env.DATABASE})
+}));
+
 app.use('/', router());
 
-app.listen(5000);
+app.listen(process.env.PORT);
