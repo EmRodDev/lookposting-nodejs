2972a56ede2fec52497df3be89e21bf4add773e9
EmRodr <emanuelrodriguez203@gmail.com>
2024-01-23 19:38:52 -0300

    Parameterized the job positions (vacancies)

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,17 @@
-exports.showJobs = (req, res) => {
+const mongoose = require("mongoose");
+const Vacancy = mongoose.model('Vacancy');
+
+exports.showJobs = async (req, res, next) => {
+
+    const vacancies = await Vacancy.find().lean();
+
+    if(!vacancies) return next();
+
     res.render('home',{
         namePage: 'LookPosting',
         tagline: 'Find and publish jobs for web developers',
         bar: true,
-        button: true
+        button: true,
+        vacancies
     });
 };
