cde25387274f9cdbd9274c4c15fc75dc3d256af4
EmRodr <emanuelrodriguez203@gmail.com>
2024-01-25 13:47:04 -0300

    selectedSkills function added

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const skills = document.querySelector('.list-knowledges');
     if (skills){
         skills.addEventListener('click', addSkills);
+
+        //Once we are on edit, call the function
+        selectedSkills();
     }
 });
 
@@ -22,4 +25,16 @@ const addSkills = (e) => {
 
     const skillsArray = [...skills];
     document.querySelector('#skills').value = skillsArray;
+}
+
+const selectedSkills = () => {
+    const selected = Array.from(document.querySelectorAll('.list-knowledges .active'));
+
+    selected.forEach(selection => {
+        skills.add(selection.textContent);
+    })
+    //Put it on the hidden
+    const skillsArray = [...skills];
+    document.querySelector('#skills').value = skillsArray;
+
 }
